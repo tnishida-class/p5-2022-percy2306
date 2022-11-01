1b3f65437ac0f0f48a9b4a5dbac4e167a39430f1
percy2306 <t.sway.tmpp@gmail.com>
2022-11-01 11:55:20 +0900

    11/1
    11/1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -2,7 +2,7 @@
 // 吹き出しの位置、背景色 etc. を関数 balloon の引数で指定できるようにしてみよう
 // 吹き出しにしっぽを付けてみよう
 function setup(){
-  createCanvas(400, 400);
+  createCanvas(1200, 1200);
   background(255);
   balloon("I love keyakizaka46");
 }
@@ -10,9 +10,12 @@ function setup(){
 function balloon(t){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 2;
+  let p = 20;
+  let rect_w =  w + p * 2;
+  let rect_h =  h + p * 2;
   fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  rect(p, p, rect_w, rect_h);
   fill(255);
-  text(t, p, h + p);
+  textAlign(CENTER,CENTER);
+  text(t, p+rect_w/2, p+ (rect_h)/2);
 }
